a4a3d806fd144c5af477b85bbbe52fa66ecdb9aa
Priyanshu <prynsu@yahoo.com>
2020-11-30 23:01:34 +0530

    feat: made the banana language translator app

---
+++ app.js
@@ -0,0 +1,43 @@
+const userText = document.querySelector(".user-text");
+const translateBtn = document.querySelector(".translate-btn");
+const translatedTxt = document.querySelector(".translated-text");
+
+translateBtn.addEventListener("click", translate);
+
+    // Sample text for checking API
+    // const text1 = "Priyanshu"
+    // const text2 = "Priyam"
+    // const text3 = "Amit"
+
+function translate(){
+    inputText = userText.value;
+    // console.log(inputText);
+    // translatedTxt.innerHTML += `<p>${inputText}</p>`;
+    // Add automated trial code here
+
+    let url = "https://api.funtranslations.com/translate/minion.json?text=" + inputText
+    fetch(url)
+    .then(function responeHandler(response) {return response.json()})
+    .then(function logJSON(json) {console.log(json)
+        bananaText = json.contents.translated;
+        translatedTxt.innerHTML += `<p>${bananaText}</p>`;
+    }
+    )
+}
+
+
+/* THE TRIAL CODE
+
+function autoCheck(text){
+        
+    let url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=" + text
+    fetch(url)
+    .then(function responeHandler(response) {return response.json()})
+    .then(function logJSON(json) {console.log(json)}
+    )}
+
+    autoCheck(text1)
+    autoCheck(text2)
+    autoCheck(text3)
+
+*/
