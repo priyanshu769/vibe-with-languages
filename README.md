73c4363553d9b4fcedeb3a7e3b0f774eb6501d7c
Priyanshu <prynsu@yahoo.com>
2020-12-04 00:38:23 +0530

    Initialising the app

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
-# Banana Translator App
+# Vibe With Languages App
 
-This app will translate your english language that you wrote to Banana language which is used by Minions.
+This app will translate your english language that you wrote to two other languages.
 
-I'm sure that if you love Minion's movies like "Minion" and "Despicable Me (Parts)", you'l love this app too.
+This app is made with FunTranslatin APIs.
 
-This app is made with Minion Translator API.
-
-[**Play with the App**](https://translatetobanana.netlify.app/)
+[**Play with the App**](https://vibewithlangs.netlify.app/)
 
 ## Improvements I want to do in the style (if you can help)
-1. Make it's input area little appealing and put the place holder in the top left.
 1. Fix the footer to the bottom of the web page.
 
 I would love your feedback. [Tweet to me](https://twitter.com/priyanshu769)
--- app.js
+++ app.js
@@ -1,35 +1,74 @@
 const userText = document.querySelector(".user-text");
-const translateBtn = document.querySelector(".translate-btn");
+const grootBtn = document.querySelector(".groot");
+const yodaBtn = document.querySelector(".yoda");
+const piratesBtn = document.querySelector(".pirates");
 const translatedTxt = document.querySelector(".translated-text");
 
-translateBtn.addEventListener("click", translate);
+grootBtn.addEventListener("click", translateToGroot);
+yodaBtn.addEventListener("click", translateToYoda);
+piratesBtn.addEventListener("click", translateToPirates);
 
-    // Sample text for checking API
-    // const text1 = "Priyanshu"
-    // const text2 = "Priyam"
-    // const text3 = "Amit"
+const testURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text="
+const grootURL = "https://api.funtranslations.com/translate/groot.json?text="
+const yodaURL = "https://api.funtranslations.com/translate/yoda.json?text="
+const piratesURL = "https://api.funtranslations.com/translate/pirate.json?text="
 
-function translate(){
+
+function translateToGroot() {
     inputText = userText.value;
-    // console.log(inputText);
-    // translatedTxt.innerHTML += `<p>${inputText}</p>`;
-    // Add automated trial code here
 
-    let url = "https://api.funtranslations.com/translate/minion.json?text=" + inputText
+    let url = grootURL + inputText
     fetch(url)
-    .then(function responeHandler(response) {return response.json()})
-    .then(function logJSON(json) {console.log(json)
-        bananaText = json.contents.translated;
-        translatedTxt.innerHTML += `<p class="output">${bananaText}</p>`;
+    .then(function responeHandler(response) { return response.json() })
+        .then(function logJSON(json) {
+            console.log(json)
+            grootText = json.contents.translated;
+            translatedTxt.innerHTML = `<p class="output">${grootText}</p>`;
+        }).catch(function errorHandling(error) {
+            alert("Server Error!!!")
+            translatedTxt.innerHTML += `<p class="output invalid">Server is facing a problem!</p>`
+        })
     }
-    )
-}
+    
+    function translateToYoda() {
+        inputText = userText.value;
+        
+    let url = yodaURL + inputText
+    fetch(url)
+        .then(function responeHandler(response) { return response.json() })
+        .then(function logJSON(json) {
+            console.log(json)
+            yodaText = json.contents.translated;
+            translatedTxt.innerHTML = `<p class="output">${yodaText}</p>`;
+        }).catch(function errorHandling(error) {
+            alert("Server Error!!!")
+            translatedTxt.innerHTML += `<p class="output invalid">Server is facing a problem!</p>`
+        })
+    }
+    
+    function translateToPirates() {
+        inputText = userText.value;
+    
+        let url = piratesURL + inputText
+        fetch(url)
+            .then(function responeHandler(response) { return response.json() })
+            .then(function logJSON(json) {
+                console.log(json)
+                piratesText = json.contents.translated;
+                translatedTxt.innerHTML = `<p class="output">${piratesText}</p>`;
+            }).catch(function errorHandling(error) {
+                alert("Server Error!!!")
+                translatedTxt.innerHTML += `<p class="output invalid">Server is facing a problem!</p>`
+            })
+        }
 
 
 /* THE TRIAL CODE
 
+https://api.funtranslations.com/translate/minion.json?text=
+
 function autoCheck(text){
-        
+
     let url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=" + text
     fetch(url)
     .then(function responeHandler(response) {return response.json()})
